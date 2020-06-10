aecbdab26b2fe57893df547447f18e3b4993b767
Grant Ho <55564015+gho19@users.noreply.github.com>
2020-06-09 20:52:58 -0600

    Initial Commit

---
+++ js/navbar.js
@@ -0,0 +1,13 @@
+<!---------------------------------------------------->
+<!-------------------- NAVBAR JS --------------------->
+<!--------------------DO NOT EDIT--------------------->
+<!---------------------------------------------------->
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
